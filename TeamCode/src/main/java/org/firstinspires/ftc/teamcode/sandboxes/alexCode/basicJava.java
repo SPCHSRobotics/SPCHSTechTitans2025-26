623e72ae74f17e00223598868600376781274b07
AlexHx11 <alexhaha555@gmail.com>
2025-10-22 15:50:39 -0400

    hello WOrld?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandboxes/alexCode/basicJava.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.sandboxes.alexCode;
+
+public class basicJava {
+    public void helloWorld() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        basicJava obj = new basicJava();
+        obj.helloWorld();
+    }
+}
