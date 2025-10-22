2889b4d4049fd5d0855716eb115f6e18fabdbdc3
Ftelon22 <Fiedland42@gmail.com>
2025-10-22 15:46:33 -0400

    Update fiedlandMain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandboxes/fiedlandCode/fiedlandMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandboxes/fiedlandCode/fiedlandMain.java
@@ -1,4 +1,11 @@
 package org.firstinspires.ftc.teamcode.sandboxes.fiedlandCode;
 
 public class fiedlandMain {
+    public void helloworld() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        fiedlandMain obj = new fiedlandMain();
+        obj.helloworld();
+    }
 }
